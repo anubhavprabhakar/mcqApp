360235b1d16bc79ac57d2c590077865e0611d96b
anubhavprabhakar <anu.devjob@gmail.com>
2022-08-26 22:16:34 +0530

    Added basic functionality to buttons
    - tapping correct/wrong answer button shows respective message
    
    - RESET button removes any message in the answer display area

--- app/src/main/java/com/example/mcqapp/MainActivity.kt
+++ app/src/main/java/com/example/mcqapp/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.mcqapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonA: Button = findViewById(R.id.button)
+        val buttonB: Button = findViewById(R.id.button2)
+        val buttonC: Button = findViewById(R.id.button3)
+        val buttonD: Button = findViewById(R.id.button4)
+        val buttonReset: Button = findViewById(R.id.button5)
+
+        val answerdisplay: TextView = findViewById(R.id.textView3)
+
+        buttonA.setOnClickListener{
+            answerdisplay.text = "Your answer: A\nWrong answer. Please try again."
+        }
+        buttonB.setOnClickListener{
+            answerdisplay.text = "Your answer: B\nCongrajulashons, correct answer!"
+        }
+        buttonC.setOnClickListener{
+            answerdisplay.text = "Your answer: C\nWrong answer. Please try again."
+        }
+        buttonD.setOnClickListener{
+            answerdisplay.text = "Your answer: D\nWrong answer. Please try again."
+        }
+        buttonReset.setOnClickListener{
+            answerdisplay.text = ""
+        }
     }
 }
